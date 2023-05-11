458b6db9cb0a6a566acfef64a4947cdd14af4c28
Ibrahima Ciss <bionik6@gmail.com>
2023-05-11 18:25:17 +0000

    Initial Commit

---
+++ AppDevConTCA/AppDevConTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevConTCAApp.swift
+//  AppDevConTCA
+//
+//  Created by Ibrahima Ciss on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppDevConTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
